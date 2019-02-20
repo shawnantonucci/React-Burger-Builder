4d6a533928cc7c883512d95e614cdd643b272882
shawn <shawnantonucci2018@gmail.com>
2019-02-20 11:01:13 -0800

    disabled less button if no type is available

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from "react";
 
-import classes from './BuildControl.module.css';
+import classes from "./BuildControl.module.css";
 
 const buildControl = props => (
-    <div className={classes.BuildControl}>
-        <div className={classes.Label}>{props.label}</div>
-        <button className={classes.Less}>Less</button>
-        <button className={classes.More}>More</button>
-    </div>
+  <div className={classes.BuildControl}>
+    <div className={classes.Label}>{props.label}</div>
+    <button
+      className={classes.Less}
+      onClick={props.remove}
+      disabled={props.disabled}
+    >
+      Less
+    </button>
+    <button
+      className={classes.More}
+      onClick={props.added}
+    >
+      More
+    </button>
+  </div>
 );
 
 export default buildControl;
